d4fe91feb2a37bedca86de413646cce6156d85d2
wafsek <saraibaljit@gmail.com>
2018-09-04 17:43:26 +0200

    Added the initial documentation.

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,12 @@
 $(document).ready(function() {
     var cites = ["London", "Oslo", "Minsk"];
 
+
+    /**
+     * Gets the weather for the given city name and edits the dom.
+     * @param cityname - The name of the city to get the weather of.
+     * @param index - The index of the city in both the cities and in the elements in the dom.
+     */
     function getCitisWeather(cityname, index) {
         $.getJSON( "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&units=metric&appid=ac3e17cf0bdb7b77a12d75c287c609f7", function( data ) {
             var imagename = data.weather[0].description;
@@ -17,12 +23,20 @@ $(document).ready(function() {
         });
     }
 
+    /**
+     * Adds a new city.
+     * @param cityname - The name of the city to be added.
+     * @param index - The index of the city. NOTE. The index HAS to be the last one and CANNOT be any other.
+     */
     function addNewCity(cityname, index){
         console.log(cityname);
         $( ".weather-widget" ).first().clone().appendTo( "#weather-widgets" );
         getCitisWeather(cityname,index);
     }
 
+    /**
+     * Setting up for the initial cities.
+     */
     function addInitialCities() {
         getCitisWeather("London",0);
         for (i = 1; i < cites.length; i++) {
@@ -31,6 +45,10 @@ $(document).ready(function() {
     }
 
     addInitialCities();
+
+    /**
+     * Updates the weather for all the cities every minute.
+     */
     setInterval(function() {
         for (var i in cites) {
             getCitieWeather(cites[i],i);
